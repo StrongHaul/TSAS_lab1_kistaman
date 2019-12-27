59c30efdbc3a3dbca23dafdeb8b159ce31339370
StrongHaul <usermage@gmail.com>
2019-12-27 22:39:18 +0500

    Initial commit

---
+++ server/lab 1/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace lab_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.components = new System.ComponentModel.Container();
+			this.button2 = new System.Windows.Forms.Button();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.z1 = new ZedGraph.ZedGraphControl();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button2
+			// 
+			this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button2.Location = new System.Drawing.Point(933, 12);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(111, 35);
+			this.button2.TabIndex = 7;
+			this.button2.Text = "Запустить";
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.AllowUserToDeleteRows = false;
+			this.dataGridView1.AllowUserToResizeColumns = false;
+			this.dataGridView1.AllowUserToResizeRows = false;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column3,
+            this.Column1,
+            this.Column4,
+            this.Column5});
+			this.dataGridView1.Location = new System.Drawing.Point(12, 62);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.ReadOnly = true;
+			this.dataGridView1.RowHeadersWidth = 4;
+			this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.dataGridView1.Size = new System.Drawing.Size(371, 333);
+			this.dataGridView1.TabIndex = 16;
+			this.dataGridView1.Visible = false;
+			// 
+			// Column3
+			// 
+			this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+			this.Column3.HeaderText = "№";
+			this.Column3.MinimumWidth = 6;
+			this.Column3.Name = "Column3";
+			this.Column3.ReadOnly = true;
+			// 
+			// Column1
+			// 
+			this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+			this.Column1.HeaderText = "Истинная T холодильника";
+			this.Column1.MinimumWidth = 6;
+			this.Column1.Name = "Column1";
+			this.Column1.ReadOnly = true;
+			// 
+			// Column4
+			// 
+			this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+			this.Column4.HeaderText = "Сглаженная T холодильника";
+			this.Column4.MinimumWidth = 6;
+			this.Column4.Name = "Column4";
+			this.Column4.ReadOnly = true;
+			// 
+			// Column5
+			// 
+			this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+			this.Column5.HeaderText = "Температура холодильника от датчика";
+			this.Column5.MinimumWidth = 6;
+			this.Column5.Name = "Column5";
+			this.Column5.ReadOnly = true;
+			// 
+			// z1
+			// 
+			this.z1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.z1.Location = new System.Drawing.Point(0, 0);
+			this.z1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.z1.Name = "z1";
+			this.z1.ScrollGrace = 0D;
+			this.z1.ScrollMaxX = 0D;
+			this.z1.ScrollMaxY = 0D;
+			this.z1.ScrollMaxY2 = 0D;
+			this.z1.ScrollMinX = 0D;
+			this.z1.ScrollMinY = 0D;
+			this.z1.ScrollMinY2 = 0D;
+			this.z1.Size = new System.Drawing.Size(1056, 413);
+			this.z1.TabIndex = 18;
+			// 
+			// timer1
+			// 
+			this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+			this.AutoScroll = true;
+			this.AutoSize = true;
+			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.BackColor = System.Drawing.SystemColors.ButtonFace;
+			this.ClientSize = new System.Drawing.Size(1056, 413);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.z1);
+			this.Controls.Add(this.dataGridView1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.Text = "Вентилятор";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private ZedGraph.ZedGraphControl z1;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+	}
+}
+
---
+++ server/lab 1/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace lab_1
+{
+	public partial class Form1 : Form
+	{
+		public double[] Tsreda = new double[1];         // истинная температура среды
+		public double[] Filtered = new double[] { 18 };       // сглаженная температура среды
+
+		public double ventilator = 50,         // частота вращения вентилятора
+			vTeds,               // термоэдс
+			vTsreda = 23,         // температура среды
+			t_sensor,
+			filtered;
+		public bool speed2 = true;        // усиленный режим вентилятора вкл
+		public double Tmax = 25,        // верхний гистерезис
+			Tmin = 17;                  // нижний гистерезис
+		public int h = 4;       // шаг сглаживания
+		Random random = new Random();
+		GraphPane pane1;     // объект для рисования графиков
+		LineItem line_chastota, line_termoeds, line_tistinnay, line_t_datchik, line_filtered;   // линии
+		PointPairList point_chastota = new PointPairList();
+		PointPairList point_termoeds = new PointPairList();
+		PointPairList point_t_istionnay_sreda = new PointPairList();
+		PointPairList point_sensor = new PointPairList();
+		PointPairList point_filtered = new PointPairList();       // списки с точками
+		int t = 0;              // счетчик времени
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			pane1 = z1.GraphPane;
+			z1.GraphPane.CurveList.Clear();
+
+			z1.IsShowPointValues = true;
+			z1.IsEnableHZoom = true;
+			z1.IsEnableVZoom = true;
+
+			pane1.XAxis.Title.IsVisible = false;
+			pane1.YAxis.Title.IsVisible = false;
+			pane1.XAxis.Scale.IsSkipFirstLabel = true;
+			pane1.XAxis.Scale.IsSkipLastLabel = true;
+			pane1.XAxis.Scale.IsSkipCrossLabel = true;
+			pane1.YAxis.Scale.IsSkipLastLabel = true;
+			pane1.YAxis.Scale.IsSkipCrossLabel = true;
+			pane1.Title.IsVisible = false;
+
+			//pane1.XAxis.Cross = 0.0;
+			//pane1.YAxis.Cross = 0.0;
+
+			pane1.XAxis.Scale.MinAuto = true;
+			pane1.XAxis.Scale.MaxAuto = true;
+			pane1.YAxis.Scale.MinAuto = true;
+			pane1.YAxis.Scale.MaxAuto = true;
+
+			pane1.IsBoundedRanges = true;    // при автоматическом подборе масштаба нужно учитывать только видимый интервал графика
+
+			line_chastota = z1.GraphPane.AddCurve("Частота вращения ветилятора (об/с)", point_chastota, Color.Orange, SymbolType.None);      // частота вращения
+			line_chastota.Line.Width = 2;
+			line_chastota.Line.IsSmooth = true;
+
+			line_t_datchik = z1.GraphPane.AddCurve("Температура в комнате (датчик)", point_sensor, Color.Red, SymbolType.None);      // температура (датчик) с погрешностью
+			line_t_datchik.Line.Width = 2;
+			line_t_datchik.Line.IsSmooth = true;
+
+			line_termoeds = z1.GraphPane.AddCurve("ЭДС", point_termoeds, Color.Blue, SymbolType.None);       // эдс
+			line_termoeds.Line.Width = 2;
+			line_termoeds.Line.IsSmooth = true;
+
+			line_tistinnay = z1.GraphPane.AddCurve("Истинная температура среды", point_t_istionnay_sreda, Color.Green, SymbolType.None);      // истинная температура
+			line_tistinnay.Line.Width = 2;
+			line_tistinnay.Line.IsSmooth = true;
+
+			line_filtered = z1.GraphPane.AddCurve("Сглаженная температура от датчика", point_filtered, Color.Purple, SymbolType.None);      // сглаженный график
+			line_filtered.Line.Width = 2;
+			line_filtered.Line.IsSmooth = true;
+
+
+			z1.AxisChange();
+			z1.Invalidate();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			timer1.Stop();
+			timer1.Interval = 1000;
+		}
+
+		private void button2_Click(object sender, EventArgs e)      // нажимаем на кнопку запустить 
+		{
+			timer1.Start();
+			button2.Enabled = false;
+
+		}
+
+		private void Timer1_Tick(object sender, EventArgs e)        // таймер
+		{
+			t++;
+			Climate();
+			Sensor();
+			Processing();
+		}
+
+		private void Climate()
+		{
+
+			if (vTsreda >= Tmax)
+			{
+				speed2 = true;
+			}
+
+			if (vTsreda <= Tmin)
+			{
+				speed2 = false;
+			}
+			if (speed2 == true)      // если вторая скорость вентилятора включена
+			{
+				ventilator -= Math.Abs(30 - ventilator) / 3;
+				double dT = Math.Log(Math.Abs(vTsreda - (Tmin - 2)));
+				vTsreda -= dT;
+			}
+			else
+			{
+				ventilator += Math.Abs(50 - ventilator) / 3;
+				double dT = Math.Log(Math.Abs(vTsreda - (Tmax + 1)));
+				vTsreda += dT;
+			}
+		}
+
+		private void Sensor()    //  датчик, выдает термоэдс
+		{
+			vTeds = vTsreda * 0.2;
+			t_sensor = vTsreda + random.Next(-1, 2);
+		}
+
+		private void Processing()   // получить данные
+		{
+			Array.Resize(ref Tsreda, Tsreda.Length + 1);
+
+			Tsreda[Tsreda.Length - 1] = vTsreda;
+
+			filtered = Smooth();
+
+			this.Invoke(new MethodInvoker(delegate
+				{
+					dataGridView1.Rows.Add(dataGridView1.Rows.Count + 1, null, filtered, null);
+					dataGridView1.FirstDisplayedCell = dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[0];
+					Graphics();
+				}));
+		}
+
+		private double Smooth()       // сглаживание данных от датчика
+		{
+			if (dataGridView1.Rows.Count <= h)
+				return 0;
+			Array.Resize(ref Filtered, Filtered.Length + 1);    // увеличили длину массива Filtered на 1
+			double filter_current = Filtered[Filtered.Length - 2];      // последнее отфильтрованное значение
+			double teds_Current_Minus_Step = Tsreda[Tsreda.Length - 1 - h];
+			double filter_next = filter_current - (teds_Current_Minus_Step / h) + (Tsreda[Tsreda.Length - 1] / h);    // новое отфильтрованное значение
+			Filtered[Filtered.Length - 1] = filter_next;
+
+			return filter_next;
+		}
+
+		private void Graphics()     // отображение графика
+		{
+			point_chastota.Add(t, ventilator);
+			point_termoeds.Add(t, vTeds);
+			point_filtered.Add(t, filtered);
+			point_t_istionnay_sreda.Add(t, vTsreda);
+			point_sensor.Add(t, t_sensor);
+
+			z1.AxisChange();
+			z1.Invalidate();
+		}
+
+	}
+
+}
+
